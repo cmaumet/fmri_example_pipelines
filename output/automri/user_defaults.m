1892626297d12fb7a1b0fcb306d8488d2d32f25b
cmaumet <camille.maumet@inria.fr>
2018-11-19 14:50:44 +0100

    Add minimal user_default

---
+++ output/automri/user_defaults.m
@@ -0,0 +1,7 @@
+TR = 2.5;
+TA = 2.5;
+
+IS_CLINICAL = false;
+
+NUM_IMG_STAB = struct('sessName', {'se_fingerfootlips'},'nBeg', {0});
+NUM_IMG_TRUNC = struct('sessName', {'se_fingerfootlips'},'nEnd',{0});
