70c237e797d0e046d772d8fe0734bb5eaf7a18f0
cmaumet <camille.maumet@inria.fr>
2018-11-19 14:50:23 +0100

    No printouts

--- scripts/automri_ex.m
+++ scripts/automri_ex.m
@@ -1,9 +1,9 @@
 bids_dir = fullfile(pwd , '..', 'data', 'ds000114_R2.0.1');
 automri_dir = fullfile(pwd , '..', 'output', 'automri');
 data_dir = fullfile(automri_dir, 'data');
-mkdir_no_warn(data_dir)
+mkdir_no_warn(data_dir);
 gr_dir = fullfile(data_dir, 'gr_control');
-mkdir_no_warn(gr_dir)
+mkdir_no_warn(gr_dir);
 
 % Reorganise data according to automri structure
 sub_dirs = dir(fullfile(bids_dir, 'sub*'));
@@ -13,13 +13,13 @@ for i = 1:numel(sub_dirs)
     sub_name = strrep(sub_dirs(i).name, 'sub-', 'su_');
     
     sub_dir = fullfile(gr_dir, sub_name);
-    mkdir_no_warn(sub_dir)
+    mkdir_no_warn(sub_dir);
     
     func_dir = fullfile(sub_dir, 'functional');
-    mkdir_no_warn(func_dir)
+    mkdir_no_warn(func_dir);
 
     sess_dir = fullfile(func_dir, 'se_fingerfootlips');
-    mkdir_no_warn(sess_dir)
+    mkdir_no_warn(sess_dir);
     
     copyfile(fullfile(bids_sub_dir, 'ses-test', 'func', ...
                       [sub_dirs(i).name '_ses-test_task-fingerfootlips_bold.nii.gz']),...
@@ -27,7 +27,7 @@ for i = 1:numel(sub_dirs)
     
     
     anat_dir = fullfile(sub_dir, 'structural');
-    mkdir_no_warn(anat_dir)
+    mkdir_no_warn(anat_dir);
     
     copyfile(fullfile(bids_sub_dir, 'ses-test', 'anat', ...
                       [sub_dirs(i).name '_ses-test_T1w.nii.gz']),...
